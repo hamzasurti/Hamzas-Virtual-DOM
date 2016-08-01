2b014dbf48d9729b65aa6c2b959347fd0c41285d
hamzasurti <hamza.surti1@gmail.com>
2016-08-01 12:03:38 -0700

    added a few things. super incomplete

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 
 // converts jsx to js obj with babel transpilation
 function nodeToVD(type,props,...children) {
-    return { type, props, children}
-}
+  return { type, props: props || {}, children };}
 
 // recursivey goes through virtual DOM and creates actual DOM elements
 function createElement(node) {
@@ -11,10 +10,42 @@ function createElement(node) {
     return document.createTextNode(node);
   }
   const $el = document.createElement(node.type);
+  setProps($el, node.props);
   node.children
     .map(createElement)
     .forEach($el.appendChild.bind($el));
   return $el;
+}
+
+function setProps($target, props) {
+  Object.keys(props).forEach(name => {
+    setProp($target, name, props[name]);
+  });
+}
+
+function setBooleanProp($target, name, value) {
+  if (value) {
+    $target.setAttribute(name, value);
+    $target[name] = true;
+  } else {
+    $target[name] = false;
+  }
+}
+
+function isCustomProp(name) {
+  return false;
+}
+
+function setProp($target, name, value) {
+  if (isCustomProp(name)) {
+    return;
+  } else if (name === ‘className’) {
+    $target.setAttribute(‘class’, value);
+  } else if (typeof value === ‘boolean’) {
+    setBooleanProp($target, name, value);
+  } else {
+    $target.setAttribute(name, value);
+  }
 }
 
 function updateElement($parent, newNode, oldNode, index = 0) {
